8d23d00079ed518dbc4af239d9d299545a7dda41
behzad <behzad.rajabalipour@gmail.com>
2024-12-05 18:21:14 -0500

    new5

--- Terraform/prod/Webserver/main.tf
+++ Terraform/prod/Webserver/main.tf
@@ -48,7 +48,7 @@ resource "aws_instance" "public_instance" {
   count                       = length(data.terraform_remote_state.prod_net_tfstate.outputs.public_subnet_ids)
   ami                         = data.aws_ami.latest_amazon_linux.id
   instance_type               = lookup(var.instance_type, var.env)
-  key_name                    = length(data.aws_key_pair.existing_key.id) > 0 ? var.keyName : aws_key_pair.keyName[0].key_name
+  key_name                    = aws_key_pair.keyName.key_name    # because we used count in key_name so we have to give it's index
   security_groups             = [aws_security_group.public_instance_SG.id]
   subnet_id                   = data.terraform_remote_state.prod_net_tfstate.outputs.public_subnet_ids[count.index] # it use s3/dev/network/terraform.tfstate => outputs. you can also see it in Network/output.tf file
   associate_public_ip_address = true
@@ -104,7 +104,7 @@ resource "aws_instance" "private_instance" {
   count                       = length(data.terraform_remote_state.prod_net_tfstate.outputs.private_subnet_ids)
   ami                         = data.aws_ami.latest_amazon_linux.id
   instance_type               = lookup(var.instance_type, var.env)
-  key_name                    = length(data.aws_key_pair.existing_key.id) > 0 ? var.keyName : aws_key_pair.keyName[0].key_name
+  key_name                    = aws_key_pair.keyName.key_name    # because we used count in key_name so we have to give it's index
   security_groups             = [aws_security_group.private_instance_SG.id]
   subnet_id                   = data.terraform_remote_state.prod_net_tfstate.outputs.private_subnet_ids[count.index] # it use s3/dev/network/terraform.tfstate => outputs. you can also see it in Network/output.tf file
 
@@ -130,14 +130,8 @@ resource "aws_instance" "private_instance" {
 }
 
 resource "aws_key_pair" "keyName" {
-  count      = length(data.aws_key_pair.existing_key.id) == 0 ? 1 : 0
   key_name   = var.keyName
   public_key = file("./${var.keyName}.pub")  # Replace with the path to your public key
-}
-
-data "aws_key_pair" "existing_key" {
-  key_name = var.keyName
-  # This will look up the key pair and return an empty result if it doesn't exist
 }
 
 #security Group
